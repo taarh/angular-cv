fa59be071e115fb6719105731a3a21bb1242ac3f
taarh <arhalaitaoufik1@gmail.com>
2022-11-10 12:03:27 +0100

    1 file modified

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, CoreModule],
+  declarations: [],
 })
-export class FeatureModule { }
+export class FeatureModule {}
